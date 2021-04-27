5237047818e7785e22124b26b9eba49f310f2acf
Nathan CRUAUD <nathan.cruaud@grenoble-inp.org>
2021-04-27 18:43:05 +0200

    Init spring boot

---
+++ ProjetGestionConge/src/test/java/formation/sopra/ProjetGestionConge/ProjetGestionCongeApplicationTests.java
@@ -0,0 +1,13 @@
+package formation.sopra.ProjetGestionConge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetGestionCongeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
