5181c9e51571de299cd73e27238ee8858ed6cbde
Anaelle <anaelle.stum@soprasteria.com>
2021-04-28 11:04:51 +0200

    angular squelette

---
+++ ProjetGestionCongeAngular/src/app/routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { ConnexionComponent } from './component/connexion/connexion.component';
+import { ConsultationCongeEmpComponent } from './component/consultation-conge-emp/consultation-conge-emp.component';
+import { ConsultationCongeMangComponent } from './component/consultation-conge-mang/consultation-conge-mang.component';
+import { DemandeCongeComponent } from './component/demande-conge/demande-conge.component';
+import { HomeComponent } from './component/home/home.component';
+import { ValidationCongeComponent } from './component/validation-conge/validation-conge.component';
+
+
+export const routes: Routes = [
+  { path: 'connexion', component: ConnexionComponent },
+  { path: 'consultation-conge-emp', component: ConsultationCongeEmpComponent },
+  { path: 'consultation-conge-mang', component: ConsultationCongeMangComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'validation-conge', component: ValidationCongeComponent },
+  { path: 'demande-conge', component: DemandeCongeComponent },
+];
